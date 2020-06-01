03ca8e9abf260be1079ab17d50cf436c8fd5e0b9
j-cuong <gcuo3@yahoo.com>
2020-06-01 17:44:38 -0400

    Updates

---
+++ precache-manifest.8a57e5a5485179b7c1228267a91d5792.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2633a282bf5682e7d0a2baa2ba6cc253",
+    "url": "/Chordian/index.html"
+  },
+  {
+    "revision": "a530d562b2a0bb4c7971",
+    "url": "/Chordian/static/css/main.1565e480.chunk.css"
+  },
+  {
+    "revision": "d2757209788eb58ee080",
+    "url": "/Chordian/static/js/2.aa749b2f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Chordian/static/js/2.aa749b2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a530d562b2a0bb4c7971",
+    "url": "/Chordian/static/js/main.d56bb804.chunk.js"
+  },
+  {
+    "revision": "9c214f435021c6bc713b",
+    "url": "/Chordian/static/js/runtime-main.bb2e5aa5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Chordian/static/media/logo.5d5d9eef.svg"
+  }
+]);
